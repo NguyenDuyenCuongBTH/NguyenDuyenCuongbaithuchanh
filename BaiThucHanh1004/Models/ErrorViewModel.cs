a8f8e8698b74e8ba51d48ce7d49ee0ed7ecacbb8
NDC114 <duyencuongmtpvip2001@gmail.com>
2023-06-20 17:03:19 +0700

    commit note

--- BaiThucHanh1004/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace BaiThucHanh1004.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
--- BaiThucHanh1004/Models/Student.cs
+++
@@ -1,9 +0,0 @@
-namespace BaiThucHanh1004.Models
-{
-    public class Student 
-    {
-        public string StudentID { get; set; }
-        public string StudentName { get; set; }
-        public string Address { get; set; }
-    }
-}
